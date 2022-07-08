a81cf8afa8e0d98a2cd75369eb7d487d2d49a453
Martincz Gao <martincz@mokeedev.com>
2022-07-08 17:19:02 +0800

    cedric: mokee-ify
    Change-Id: Ib2e0ca8aae58a8e5a9599c9a22df76a9ede0e0f6

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_cedric.mk
+    $(LOCAL_DIR)/mokee_cedric.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_cedric-user \
-    lineage_cedric-userdebug \
-    lineage_cedric-eng
+    mokee_cedric-user \
+    mokee_cedric-userdebug \
+    mokee_cedric-eng
